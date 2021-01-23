3db22f2edfe239fc13ec549b04512f8c7f1b4e18
Anders Busch <andersbusch@gmail.com>
2021-01-23 22:30:56 +0100

    matrix and matrix transform

---
+++ webgl/matrix3.js
@@ -0,0 +1,60 @@
+
+export function translation(tx, ty) {
+        return [
+            1,  0,  0,
+            0,  1,  0,
+            tx, ty, 1
+        ];
+    };
+
+export function rotation(radians) {
+    let c = Math.cos(radians)
+    let s = Math.sin(radians)
+
+    return [
+        c, -s, 0,
+        s,  c, 0,
+        0,  0, 1,
+    ];
+};
+
+export function scaling(sx, sy) {
+    return [
+        sx,  0,  0,
+        0,  sy,  0,
+        0,   0,  1,
+    ];
+};
+
+export function multiply(mat_a, mat_b) {
+    let a00 = mat_a[0 * 3 + 0];
+    let a01 = mat_a[0 * 3 + 1];
+    let a02 = mat_a[0 * 3 + 2];
+    let a10 = mat_a[1 * 3 + 0];
+    let a11 = mat_a[1 * 3 + 1];
+    let a12 = mat_a[1 * 3 + 2];
+    let a20 = mat_a[2 * 3 + 0];
+    let a21 = mat_a[2 * 3 + 1];
+    let a22 = mat_a[2 * 3 + 2];
+    let b00 = mat_b[0 * 3 + 0];
+    let b01 = mat_b[0 * 3 + 1];
+    let b02 = mat_b[0 * 3 + 2];
+    let b10 = mat_b[1 * 3 + 0];
+    let b11 = mat_b[1 * 3 + 1];
+    let b12 = mat_b[1 * 3 + 2];
+    let b20 = mat_b[2 * 3 + 0];
+    let b21 = mat_b[2 * 3 + 1];
+    let b22 = mat_b[2 * 3 + 2];
+
+    return [
+        b00 * a00 + b01 * a10 + b02 * a20,
+        b00 * a01 + b01 * a11 + b02 * a21,
+        b00 * a02 + b01 * a12 + b02 * a22,
+        b10 * a00 + b11 * a10 + b12 * a20,
+        b10 * a01 + b11 * a11 + b12 * a21,
+        b10 * a02 + b11 * a12 + b12 * a22,
+        b20 * a00 + b21 * a10 + b22 * a20,
+        b20 * a01 + b21 * a11 + b22 * a21,
+        b20 * a02 + b21 * a12 + b22 * a22,
+    ];
+}
---
+++ webgl2-matrix2d-transform/main.js
@@ -0,0 +1,173 @@
+import * as webglUtils from '../webgl/utils.js'
+import * as m3 from '../webgl/matrix3.js'
+
+//@ts-check
+
+function setGeometry(gl) {
+    // drawing an F out of three bawxes
+    gl.bufferData(
+        gl.ARRAY_BUFFER,
+        new Float32Array([
+            // left box
+            0, 0,
+            30, 0,
+            0, 150,
+            0, 150,
+            30, 0,
+            30, 150,
+
+            // top box
+            30, 0,
+            100, 0,
+            30, 30,
+            30, 30,
+            100, 0,
+            100, 30,
+
+            // middle box
+            30, 60,
+            67, 60,
+            30, 90,
+            30, 90,
+            67, 60,
+            67, 90
+
+        ]),
+        gl.STATIC_DRAW
+    );
+}
+
+/**
+ * scene drawing. the vertex shader translates the F, while input data remains the same
+ * @param {WebGL2RenderingContext} gl 
+ * @param {WebGLVertexArrayObject} vao 
+ */
+function drawScene(gl, vao, position, data) {
+
+    gl.viewport(0, 0, gl.canvas.width, gl.canvas.height)
+    gl.clearColor(0, 0, 0, 0)
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT) // clear screen space with an transparent value
+
+    gl.uniform2f(position.resolutionPtr, gl.canvas.width, gl.canvas.height);
+
+    gl.bindVertexArray(vao)
+
+    gl.uniform4fv(position.colorPtr, data.color)
+
+    let translationMatrix = m3.translation(data.translation[0], data.translation[1])
+    let rotationMatrix = m3.rotation(data.rotation)
+    let scaleMatrix = m3.scaling(data.scale[0], data.scale[1])
+
+    let matrix = m3.multiply(translationMatrix, rotationMatrix)
+    matrix = m3.multiply(matrix, scaleMatrix)
+
+    gl.uniformMatrix3fv(position.transformPtr, false, matrix)
+
+    let triangleCount = 18
+    let offset = 0
+    gl.drawArrays(gl.TRIANGLES, offset, triangleCount)
+}
+
+
+/**
+ * @param {string} vertexShaderSource
+ * @param {string} fragmentShaderSource
+ * @returns {void}
+ */
+function main(vertexShaderSource, fragmentShaderSource) {
+    if (!vertexShaderSource || !fragmentShaderSource) {
+        console.error("Could not find either shader code")
+        return
+    }
+
+    let translation = [0, 0]
+    let color = [Math.random(), Math.random(), Math.random(), 1]
+    let rotation = 0
+    let scale = [1, 1]
+
+
+    /* Step 1: Prepare WebGL context and fetch from DOM */
+    const gl = webglUtils.newWebGL2Context('#canvas')
+
+    /* Step 2: Create the webgl program using shaders */
+    const program = new webglUtils.WebGl2Program(gl, vertexShaderSource, fragmentShaderSource)
+
+    const posPtr = program.getAttributeLocation("a_position")
+    const resolutionPtr = program.getUniformLocation("u_resolution")
+    const colorPtr = program.getUniformLocation("u_color")
+    const transformPtr = program.getUniformLocation("u_transform")
+
+
+    const bufferObject2 = gl.createBuffer()
+    const vao = gl.createVertexArray()
+
+    gl.bindVertexArray(vao) // set vao as the active vao
+
+    gl.enableVertexAttribArray(posPtr)
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, bufferObject2)
+
+    setGeometry(gl)
+
+    gl.vertexAttribPointer(posPtr, 2, gl.FLOAT, false, 0, 0)
+
+    const redraw = () => {
+        drawScene(gl, vao,
+            {colorPtr, resolutionPtr, transformPtr},
+            {translation, color, rotation, scale}
+        )
+    }
+
+    redraw()
+
+    let ui_section = webglUtils.getUiSection()
+
+    let sliders = [
+        webglUtils.addSlider(
+            'Scale:',
+            {minValue: -3, maxValue: 3, stepValue: 0.1, currentValue: 1},
+            value => {
+                scale[0] = Number(value)
+                scale[1] = Number(value)
+                redraw()
+            }
+        ),
+        webglUtils.addSlider(
+            'Rotation:',
+            {minValue: 0, maxValue: 360},
+            value => {
+                let angleInDegrees = Number(value)
+                rotation = angleInDegrees * Math.PI / 180 
+                redraw()
+            }
+        ),
+        webglUtils.addSlider(
+            'X Translation:',
+            {minValue: 0, maxValue: 300},
+            value => {
+                translation[0] = Number(value)
+                redraw()
+            }
+        ),
+        webglUtils.addSlider(
+            'Y Translation:',
+            {minValue: 0, maxValue: 200},
+            value => {
+                translation[1] = Number(value)
+                redraw()
+            }
+        )
+    ];
+    sliders.forEach((value) => {
+        ui_section.appendChild(value)
+    })
+}
+
+window.onload = () => {
+    Promise.all([
+        fetch('vertex.glsl').then(response => response.text()),
+        fetch('fragment.glsl').then(response => response.text())
+    ]).then(([vertexShaderCode, fragmentShaderCode]) => {
+        main(vertexShaderCode, fragmentShaderCode)
+    })
+}
